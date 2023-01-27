7823ddba6ccb52df29f618f615026b7a26838c17
Innocentsax <innocentcharlesudo@gmail.com>
2023-01-27 13:45:33 +0100

    Day_3 of Python

---
+++ Day_3.py
@@ -0,0 +1,39 @@
+//Concatenation in Python
+
+name = input("What's you name: ")
+print()
+cohort = input("What's your Cohort: ")
+print()
+exp = input("Kindly tell us your experience in ALX AFRICA so far: ")
+print()
+advice = input("What advice do you have to tell the young African out there about ALX AFRICA: ")
+print()
+team = input("Would you like to be a part of my team, where we write one program each day? ")
+
+print()
+print()
+print("Hey😘!! You are",name, "and you are from Cohort",cohort,"."
+        "Your experience so far as being" ,exp,
+        ". Your advice for the young guy:", advice, 
+        ".wow!!😁,:",
+        team, "You are Amazing!!")
+
+
+
+urName = input("What's your name?: ")
+urLunch = input("What are you having for lunch: ")
+lunchnum = input("What's your lunch number:")
+
+print()
+print("Awesome! Your name is",urName, "and you are about to enjoy",urLunch,"for lunch with these lunch number",lunchnum)
+
+print()
+food = input("Name a food: ")
+plant = input("Name a type of plant: ")
+med = input("Name a method of cooking: ")
+burn = ("What word describes burned food: ")
+diy = input("Name a DIY item: ")
+
+print()
+menu = input("Type MENU to see our Menu: ")
+print(food, plant, med, burn, diy)
