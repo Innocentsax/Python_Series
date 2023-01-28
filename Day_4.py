a81a40811f8bb3822ea7ca21e3aa822890c84fea
Innocentsax <innocentcharlesudo@gmail.com>
2023-01-28 15:29:20 +0100

    Day_3 of 100

---
+++ Day_4.py
@@ -0,0 +1,18 @@
+// ADDING COLOUR TO YOU OUTPUT
+
+
+name = input("What's your name: ")
+print()
+cohort = input("What's your Cohort: ")
+print()
+group = input("Would you like to join my study group? ")
+print()
+num = input("If yes! Please type your whatsapp number here: ")
+
+print()
+print()
+print("\033[33m","==> YOUR DATA <==", "\033[0m")
+print("Your name is","\033[34m", name, "\033[0m")
+print("wow!!, You are from amazing cohort:", cohort)
+print(group,"!! It will be an Awesome moment with you")
+print("\033[35m", num, "\033[0m","!! Your number will be added soonest")
