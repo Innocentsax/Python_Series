ebc314cda845bde5345d227168fce03e5cfa27cf
Innocentsax <innocentcharlesudo@gmail.com>
2023-01-29 16:00:06 +0100

    100 Days of coding in Python

---
+++ Day_5.py
@@ -0,0 +1,23 @@
+// Using IF-ELSE statement in Python!!
+
+
+myName = input("What's your name: ")
+if myName == "Innocent":
+    print("Welcome!!", myName, "you are a hero")
+else:
+    print("Invalid Name.")
+          print("please enter your name again.")
+          print()
+          print()
+          print("==> ALX AFRICA CHARACTER CREATOR <==")
+          i = input("Have you been doing your ALX project?: ")
+          if i == "Yes":
+              print("Amazing!!",myName, "You will be a great Software Engineer")
+          else:
+              print("Chai!!.. You are a Failure!.. You can never make it")
+                  print()
+                  j = input("Do you belong to PLD group?: ")
+                  if j == "Yes":
+                      print(myName, ":Your chance of growth is 100%")
+                  else:
+                      print("You are a failure, But you can still make it")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Day 1 - 100 Days of Code: How to print out Information on the console 
-Day 2 - 100 Days of Code: How to receive iput from User and store them in Variables
+Day 2 - 100 Days of Code: How to receive iput from User and store them in Variableis
+Day 3 - 100 Days of Code: Concatenation in Python.
+Day 4 - 100 Days of Code: ADDING COLOUR TO YOU OUTPUT.
+Day 5 - 100 Days of Code: Using IF-ELSE statement in Python
