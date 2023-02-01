39ef6dd16ae6df8057b6b88caf40ab9a13999d99
Innocentsax <innocentcharlesudo@gmail.com>
2023-02-01 13:54:32 +0100

    Day8_100days of coding

---
+++ Day_8.py
@@ -0,0 +1,15 @@
+// HAPPY NEW MONTH TEXT USING IF STATEMENT
+
+print("\033[35m", "==> HAPPY NEW MONTH TEXT <==", "\033[0m")
+
+ newMonth = input("Would you like to get a new month texts? Yes/No!: ")
+ print()
+ if newMonth == "Yes" or newMonth == "yes":
+     print("""Divine success, unmerited favor, and joy unspeakable shall be yours forever. Happy New Month Dear Friend!!""")
+ elif newMonth == "No" or newMonth == "no":
+     print("Eyaaaa, Sorry my Friend.")
+print()
+print()
+print()
+print("@iamINNOCENT.UDO")
+
