beaea3d090e2c28b439589491f5d570f0a5a8be1
Innocentsax <innocentcharlesudo@gmail.com>
2023-02-03 13:59:12 +0100

    Data type with ELIF STATEMENT IN PYTHON

---
+++ Day_9.py
@@ -0,0 +1,42 @@
+print("==>","\033[31m","GENERATION IDENTIFIER", "\033[0m", "<==")
+        print()
+        myAge = int(input("Kindly enter your year of Birth: "))
+        print()
+        if myAge >= 1883 and myAge <= 1900:
+            print("Hah🙄!! Lost Generation")
+        elif myAge >= 1901 and myAge <= 1927:
+            print("Boom😎!! Greatest Generation")
+        elif myAge >= 1928 and myAge <= 1945:
+            print("Yo🤐! Silent Generation")
+        elif myAge >= 1946 and myAge <= 1964:
+            print("Yeah, Baby Boomer")
+        elif myAge >= 1965 and myAge <= 1980:
+            print("We Rock😊! Generation X")
+        elif myAge >= 1981 and myAge <= 1996:
+            print("Eh✌! Millennials")
+        elif myAge >= 1997 and myAge <= 2012:
+            print("mh😍! Generation Z")
+        elif myAge > 2012:
+            print("mh😍! Generation Alpha")
+        else:
+            print("Invalid input")
+
+// Data Type IN PHYTHON
+                              print()
+                              print()
+                              print("==>","\033[31m", "DATA TYPE INT","\033[0m", "<==")
+                              print()
+                              myScore = int (input("Kindly enter your Score: "))
+                              if myScore >= 80:
+                                  print("Congratulations!! You have pass your Evaluation test for this month")
+                              else:
+                              print("kindy, Apply for the next Cohort")
+                              print()
+                              print()
+                              print("==>","\033[34m","DATA TYPE FLOAT","\033[0m", "<==")
+                              print()
+                              alxGrade = float(input("Kindly enter your ALX grade: "))
+                              if alxGrade >= 80.00:
+                              print("Congratulation!! You have pass your evaluation test these month")
+                              else:
+                              print("Kindly, Apply for the next Cohort")
