4b139c82e3e7f36c9de4eb7e63bb5b567a0f48fa
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-02-25 10:28:26 +0100

    User interface Project using Python

---
+++ Day_31.py
@@ -0,0 +1,47 @@
+def colorChange(color):
+  if color=="red":
+    return ("\033[31m")
+  elif color=="white":
+    return ("\033[0m")
+  elif color=="blue":
+    return ("\033[34m")
+  elif color=="yellow":
+    return ("\033[33m")
+  elif color == "green":
+    return ("\033[32m")
+  elif color == "purple":
+    return ("\033[35m")
+
+title = f"{colorChange('red')}={colorChange('white')}={colorChange('blue')}= {colorChange('yellow')}Music App {colorChange('blue')}={colorChange('white')}={colorChange('red')}="
+
+print(f"        {title:^35}")
+print(f"🔥▶️\t{colorChange('white')}Radio Gaga")
+print(f"\t{colorChange('yellow')}Queen")
+
+prev = "prev"
+next = "next"
+pause = "pause"
+
+print(f"{colorChange('white')}{prev:<35}")
+print(f"{colorChange('green')}{next:^35}")
+print(f"{colorChange('purple')}{pause:>35}")
+
+
+print()
+print()
+text = "WELCOME TO"
+print(f"{colorChange('white')}{text:^35}")
+text = "--  ARMBOOK  --"
+print(f"{colorChange('blue')}{text:^35}")
+text = "Definitely not a rip off"
+print(f"{colorChange('yellow')}{text:>35}")
+text = "a certain other social"
+print(f"{colorChange('yellow')}{text:>35}")
+text = "networking site"
+print(f"{colorChange('yellow')}{text:>35}")
+text = "Honest."
+print(f"{colorChange('red')}{text:^35}")
+text = "Username: "
+username = input(f"{colorChange('white')}{text:^35}")
+text = "Password: "
+username = input(f"{colorChange('white')}{text:^35}")
